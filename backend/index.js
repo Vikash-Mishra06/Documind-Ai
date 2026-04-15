09c7eba41a2055533c39101386e4dce686fc059b
Vikash Mishra <mishravikash0612@gmail.com>
2026-04-15 14:40:50 +0530

    Integrate MongoDB using mongoose

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const uploadRoutes = require("./routes/uploadRoutes");
 const queryRoutes = require("./routes/queryRoutes");
+const connectDB = require("./config/db");
 
 const app = express();
 
@@ -17,6 +18,8 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
